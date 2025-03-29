54c0db3a67cd3cbd00cc43332997a5848414eae8
sutolgaz <syntsrskay@gmail.com>
2025-03-29 22:49:28 +0200

    заработал бек(почти)

--- website/main/urls.py
+++ website/main/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 from . import views
+from .views import scan
+
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('nmap',views.nmap),
+    path('scanvus',views.scanvus),
+    path('nuclei',views.nuclei),
+    path('api/scan/', scan, name='scan'),
 ]
--- website/main/views.py
+++ website/main/views.py
@@ -1,6 +1,57 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+import subprocess
+import json
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 
 def index(request):
     return render(request, 'main/index.html')
+
+def nmap(request):
+    return render(request, 'main/nmap.html')
+
+def scanvus(request):
+    return render(request, 'main/scanvus.html')
+
+def nuclei(request):
+    return render(request, 'main/nuclei.html')
+
+@csrf_exempt
+def scan(request):
+    if request.method == 'POST':
+        try:
+            # Получаем данные из запроса
+            data = json.loads(request.body)
+            target_url = data.get('target')
+            templates = data.get('templates', [])
+
+            # Проверяем, что URL задан
+            if not target_url:
+                return JsonResponse({'error': 'URL не указан'}, status=400)
+
+            # Составляем команду для Nuclei
+            nuclei_command = ['/home/nadya/go/bin/nuclei', '-u', target_url]
+
+            # Добавляем выбранные шаблоны в команду
+            for template in templates:
+                nuclei_command.extend(['-t', f'/home/nadya/nuclei-templates/{template}.yaml'])  # Путь к шаблонам
+
+            # Запуск Nuclei
+            result = subprocess.run(
+                nuclei_command,
+                capture_output=True,
+                text=True
+            )
+
+            # Если процесс завершился с ошибкой
+            if result.returncode != 0:
+                return JsonResponse({'error': result.stderr}, status=500)
+
+            # Возвращаем результаты сканирования
+            return JsonResponse({'result': result.stdout})
+
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=500)
+    else:
+        return JsonResponse({'error': 'Неверный метод запроса'}, status=400)
